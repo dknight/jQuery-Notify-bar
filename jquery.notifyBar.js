defeb325b82eeb48bfcc689b712251cecff644a7
Dmitri <smirnov.dmitri@gmail.com>
2013-04-05 00:18:36 +0300

    Version 1.3 released.

--- jquery.notifyBar.js
+++ jquery.notifyBar.js
@@ -1,109 +1,127 @@
 /*
-* Notify Bar - jQuery plugin
+* Notify Bar - $ plugin
 *
-* Copyright (c) 2009-2010 Dmitri Smirnov
+* Copyright (c) 2009-2013 Dmitri Smirnov
 *
 * Licensed under the MIT license:
 * http://www.opensource.org/licenses/mit-license.php
 *
-* Version: 1.2.2
+* Version: 1.3
 *
 * Project home:
-* http://www.dmitri.me/blog/notify-bar
+* http://www.whoop.ee/ (Isn't ready yet)
 */
  
 /**
-* param Object
-*/
-jQuery.notifyBar = function(settings) {
-  
-  (function($) {
-    
-    var bar = notifyBarNS = {};
-    notifyBarNS.shown = false;
-     
-    if( !settings) {
-    settings = {};
-    }
-    // HTML inside bar
-    notifyBarNS.html = settings.html || "Your message here";
-     
-    //How long bar will be delayed, doesn't count animation time.
-    notifyBarNS.delay = settings.delay || 2000;
-     
-    //How long notifyBarNS bar will be slided up and down
-    notifyBarNS.animationSpeed = settings.animationSpeed || 200;
-     
-    //Use own jquery object usually DIV, or use default
-    notifyBarNS.jqObject = settings.jqObject;
-     
-    //Set up own class
-    notifyBarNS.cls = settings.cls || "";
-    
-    //close button
-    notifyBarNS.close = settings.close || false;
-    
-    if( notifyBarNS.jqObject) {
-      bar = notifyBarNS.jqObject;
-      notifyBarNS.html = bar.html();
-    } else {
-      bar = jQuery("<div></div>")
-      .addClass("jquery-notify-bar")
-      .addClass(notifyBarNS.cls)
-      .attr("id", "__notifyBar");
-    }
-         
-    bar.html(notifyBarNS.html).hide();
-    var id = bar.attr("id");
-    switch (notifyBarNS.animationSpeed) {
-      case "slow":
-      asTime = 600;
-      break;
-      case "normal":
-      asTime = 400;
-      break;
-      case "fast":
-      asTime = 200;
-      break;
-      default:
-      asTime = notifyBarNS.animationSpeed;
-    }
-    if( bar != 'object'); {
-      jQuery("body").prepend(bar);
-    }
-    
-    // Style close button in CSS file
-    if( notifyBarNS.close) {
-      bar.append(jQuery("<a href='#' class='notify-bar-close'>Close [X]</a>"));
-      jQuery(".notify-bar-close").click(function() {
-        if( bar.attr("id") == "__notifyBar") {
-          jQuery("#" + id).slideUp(asTime, function() { jQuery("#" + id).remove() });
-        } else {
-          jQuery("#" + id).slideUp(asTime);
-        }
-        return false;
-      });
-    }
-    
-  // Check if we've got any visible bars and if we have, slide them up before showing the new one
-  if($('.jquery-notify-bar:visible').length > 0) {
-    $('.jquery-notify-bar:visible').stop().slideUp(asTime, function() {
-      bar.stop().slideDown(asTime);
-    });
-  } else {
-    bar.slideDown(asTime);
-  }
-  
-  // Allow the user to click on the bar to close it
-  bar.click(function() {
-    $(this).slideUp(asTime);
-  })
-     
-  // If taken from DOM dot not remove just hide
-  if( bar.attr("id") == "__notifyBar") {
-    setTimeout("jQuery('#" + id + "').stop().slideUp(" + asTime +", function() {jQuery('#" + id + "').remove()});", notifyBarNS.delay + asTime);
-  } else {
-    setTimeout("jQuery('#" + id + "').stop().slideUp(" + asTime +", function() {jQuery('#" + id + "')});", notifyBarNS.delay + asTime);
-  }
+ * param Object
+ */
+(function ($) {
 
-})(jQuery) };
+    $.notifyBar = function(options) {
+
+        var bar = settings = {};
+        var rand = parseInt(Math.random() * 100000000);
+
+        var settings = $.extend({
+            html           : 'Your message here',
+            delay          : 2000,
+            animationSpeed : 200,
+            jqObject       : "",
+            close          : false,
+            closeText      : 'Close [X]',
+            closeOnClick   : true,
+            closeOnOver    : false
+        }, options);
+       
+
+        if( settings.jqObject) {
+            bar = settings.jqObject;
+            settings.html = bar.html();
+        } else {
+            bar = $("<div></div>")
+                   .addClass("jquery-notify-bar")
+                   .addClass(settings.cssClass)
+                   .attr("id", "__notifyBar" + rand);
+        }
+        var text_wrapper = $("<span></span>")
+                            .addClass("notify-bar-text-wrapper")
+                            .html(settings.html);
+
+        bar.html(text_wrapper).hide();
+
+        var id = bar.attr("id");
+        switch (settings.animationSpeed) {
+            case "slow":
+                asTime = 600;
+                break;
+            case "default":
+            case "normal":
+                asTime = 400;
+                break;
+            case "fast":
+                 asTime = 200;
+                 break;
+            default:
+                 asTime = settings.animationSpeed;
+        }
+        if( bar != 'object'); {
+            $("body").prepend(bar);
+        }
+        
+        // Style close button in CSS file
+        if(settings.close) {
+            bar.append($("<a href='#' class='notify-bar-close'>" + settings.closeText + "</a>"));
+            $(".notify-bar-close").click(function() {
+                if( bar.attr("id") == "__notifyBar" + rand) {
+                    $("#" + id).slideUp(asTime, function() { $("#" + id).remove() });
+                } else {
+                    $("#" + id).slideUp(asTime);
+                }
+                return false;
+            });
+        }
+            
+        // Check if we've got any visible bars and if we have,
+        // slide them up before showing the new one
+        if($('.jquery-notify-bar:visible').length > 0) {
+            $('.jquery-notify-bar:visible').stop().slideUp(asTime, function() {
+                bar.stop().slideDown(asTime);
+            });
+        } else {
+            bar.slideDown(asTime);
+        }
+        
+        // Allow the user to click on the bar to close it
+        if(settings.closeOnClick) {
+            bar.click(function () {
+                $(this).slideUp(asTime, function () {
+                    $(this).remove();
+                });
+            });
+        }
+
+        // Allow the user to move mouse on the bar to close it
+        if(settings.closeOnOver) {
+            bar.mouseover(function () {
+                 $(this).slideUp(asTime, function () {
+                    $(this).remove();
+                });
+            });
+        }
+             
+        // If taken from DOM dot not remove just hide
+        if( bar.attr("id") == "__notifyBar" + rand) {
+            setTimeout(function () {
+                $("#" + id).stop().slideUp(asTime, function () {
+                    $(this).remove();
+                });
+            }, settings.delay + asTime);
+        } else {
+            setTimeout(function () {
+                $("#" + id).stop().slideUp(asTime, function () {
+                    $(this).remove();
+                });
+            }, settings.delay + asTime);
+        }
+    };
+})( jQuery );
