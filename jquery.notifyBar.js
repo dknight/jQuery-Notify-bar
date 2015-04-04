17f17b8e51662e57c14874f8218bebc0082e3c8b
dknight <smirnov.dmitri@gmail.com>
2015-04-04 03:11:26 +0300

    Remove version from file

--- jquery.notifyBar.js
+++ jquery.notifyBar.js
@@ -6,8 +6,6 @@
 * Licensed under the MIT license:
 * http://www.opensource.org/licenses/mit-license.php
 *
-* Version: 1.5.1
-*
 * Project home:
 * http://www.whoop.ee/posts/2013/04/05/the-resurrection-of-jquery-notify-bar.html
 */
